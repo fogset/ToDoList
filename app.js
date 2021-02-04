74f7265bc7f469d3b841b02613d2c92c77329c91
fogset <zhangth1990@hotmail.com>
2021-02-04 04:05:55 -0800

    passing data to web page to server then pass back to webpage

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food","Cook Food","Eat Food"];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+
 
 
 app.get("/", function(req, res) {
@@ -13,33 +17,25 @@ app.get("/", function(req, res) {
   var currentDay = today.getDay();
   var day ="Sunday";
 
-  switch (currentDay){
-    case 0:
-    day ="Sunday"
-    break;
-    case 1:
-    day ="Monday"
-    break;
-    case 2:
-    day ="Tuesday"
-    break;
-    case 3:
-    day ="Wednesday"
-    break;
-    case 4:
-    day ="Thursday"
-    break;
-    case 5:
-    day ="Friday"
-    break;
-    case 6:
-    day ="Saturday"
-    break;
-    default:
-    console.log("Error: current day is equal to: " + currentDay);
+  var options = {
+    weekday: "long",
+    day:"numeric",
+    month:"long"
   }
-  res.render("list", {kindOfDay: day});
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindOfDay:day,
+    newListItems:items
+  });
+
 });
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 
 app.listen(3000, function() {
