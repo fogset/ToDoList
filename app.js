dc144b22c73a8aa7ae7dc2a84e09a257da920287
fogset <zhangth1990@hotmail.com>
2021-03-04 23:59:52 -0800

    check if item are empty

--- app.js
+++ app.js
@@ -34,23 +34,27 @@ const item3 = new Item({
 });
 
 const defaultItems =[item1, item2, item3];
-// Item.insertMany(defaultItems, function(err) {
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("Successfully saved default items to DB.");
-//   }
-// });
+
 
 
 app.get("/", function(req, res) {
   let day = date.getDate();
   Item.find({}, function(err, foundItems){
 
-    res.render("list", {
-      listTitle: day,
-      newListItems: foundItems
-    });
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err) {
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully saved default items to DB.");
+        }
+      });
+    }else{
+      res.render("list", {
+        listTitle: day,
+        newListItems: foundItems
+      });
+    }
   })
 
 });
